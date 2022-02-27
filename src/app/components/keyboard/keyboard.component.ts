c13d7f15d4319259532e5a46ec4dba6a0ff542be
Emmanuel <polosetroc@gmail.com>
2022-02-27 13:01:31 -0800

    Fixed css color styling for proper light and dark mode based on device preference.

--- src/app/components/keyboard/keyboard.component.ts
+++ src/app/components/keyboard/keyboard.component.ts
@@ -1,6 +1,5 @@
 import {Component, HostListener} from '@angular/core';
 import {PlayNoteService} from '../../shared/play-note.service';
-import {NoteFrequencies} from '../../enums/note-frequencies';
 
 
 @Component({
@@ -34,31 +33,17 @@ export class KeyboardComponent {
     const element = document.getElementById(key);
     if (element != null) {
       element.style.backgroundColor = 'gray';
-      element.style.color = 'white';
     }
   }
 
   animateKeyRelease(element: HTMLElement) {
-    let bgColor = 'rgb(33, 37, 41)';
-    let color = 'white';
+    const whiteIvory = '#fffff0';
+    let bgColor = '#1e2023';
     // @ts-ignore
     if (element.parentElement.id === 'white-keys-container') {
-      bgColor = 'white';
-      color = 'black';
+      bgColor = whiteIvory;
     }
     element.style.backgroundColor = bgColor;
-    element.style.color = color;
   }
 
-  /**
-   * Determines if the KeyboardEvent contains a supported key value.
-   * If so then it returns the key's frequency
-   * otherwise it returns a null string
-   *
-   * @param event : KeyboardEvent
-   */
-  isSupportedKey(event: KeyboardEvent) {
-    const key = event.key.toUpperCase();
-    return (Object.keys(NoteFrequencies).includes(`${key}`)) ? key : '';
-  }
 }
